02557efbccb3ee6413381644404046bb908454ca
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-01 02:20:05 +0530

    Update blacklist.py

--- MaviManager/modules/blacklist.py
+++ MaviManager/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.blacklist_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.warns import warn
+import MaviManager.modules.sql.blacklist_sql as sql
+from MaviManager import LOGGER, dispatcher
+from MaviManager.modules.connection import connected
+from MaviManager.modules.disable import DisableAbleCommandHandler
+from MaviManager.modules.helper_funcs.alternate import send_message, typing_action
+from MaviManager.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from MaviManager.modules.helper_funcs.extraction import extract_text
+from MaviManager.modules.helper_funcs.misc import split_message
+from MaviManager.modules.helper_funcs.string_handling import extract_time
+from MaviManager.modules.log_channel import loggable
+from MaviManager.modules.sql.approve_sql import is_approved
+from MaviManager.modules.warns import warn
 
 BLACKLIST_GROUP = 11
 
