e4e4610325f1177478142d7e32e7b73761314ee5
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-01 02:28:42 +0530

    Update users_sql.py

--- MaviManager/modules/sql/users_sql.py
+++ MaviManager/modules/sql/users_sql.py
@@ -10,8 +10,8 @@ from sqlalchemy import (
     func,
 )
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.sql import BASE, SESSION
+from MaviManager import dispatcher
+from MaviManager.modules.sql import BASE, SESSION
 
 
 class Users(BASE):
