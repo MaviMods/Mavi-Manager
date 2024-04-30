78c66e7589c072873eee422ffc31ef8e8b1b5447
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-01 02:23:07 +0530

    Update afk.py

--- MaviManager/modules/afk.py
+++ MaviManager/modules/afk.py
@@ -5,13 +5,13 @@ from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import (
+from MaviManager import dispatcher
+from MaviManager.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from FallenRobot.modules.sql import afk_sql as sql
-from FallenRobot.modules.users import get_user_id
+from MaviManager.modules.sql import afk_sql as sql
+from MaviManager.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
